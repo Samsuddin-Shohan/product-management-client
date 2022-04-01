8b8f042ec80e8c7929c75edc2bc88a1a4f453b61
Samsuddin-Shohan <shohan.cse.hstu@gmail.com>
2022-04-01 18:29:06 +0600

    get and post product activated

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,23 @@
+import axios from 'axios';
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Products = () => {
+    const [products, setProducts]=useState([]);
+
+    useEffect(()=>{
+        axios.get('http://localhost:5000/products')
+        .then(res =>setProducts(res.data))
+    },[]);
     return (
         <div>
-           <h1>This is Product</h1>
+           <h1>Product Available : {products.length} </h1>
+           <ul>
+            {
+                products.map(product => <li key={product._id}>{product.newProduct?.name} : {product.newProduct?.price} : {product.newProduct?.brand} </li>)
+            }
+           </ul>
         </div>
     );
 };
